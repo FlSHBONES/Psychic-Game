ea4498b632380792cc5719072b8a49887f584da0
TFisher <tgfisher26@gmail.com>
2018-08-18 22:07:01 -0400

    fine tune and debug pass

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,53 @@
 
-		 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var randomarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-		 var wins = 0;
-		 var losses = 0;
-		 var numGuesses = 9;
-		 var guessChoices = [];
+var wins = 0;
+var losses = 0;
+var guess = [];
+var attempts = 12;
 
-		 document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-		 	var userGuess = event.key;
+	var userGuess = event.key;
 
-		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	var computerGuess = randomarr[Math.floor(Math.random() * randomarr.length)];
 
-		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-		 	
+	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-			if (options.indexOf(userGuess) > -1) {
-			
-				if (userGuess === computerGuess) {
-					wins++;
-					numGuesses = 9;
-					guessChoices = [];
-				}
 
-				if (userGuess != computerGuess) {
-					numGuesses --;
-					guessChoices.push(userGuess);
-				}
+	if (options.indexOf(userGuess) > -1) {
 
-				if (numGuesses === 0) {
+		if (userGuess === computerGuess) {
+			wins++;
+			attempts = 12;
+			guess = [];
+			alert("You're a mind reader! You Win!");
+		}
 
-				numGuesses = 9;
-				losses ++;
-				guessChoices = [];
-                alert("You Lose! " + "The letter was " + );
+		if (userGuess != computerGuess) {
+			attempts--;
+			guess.push(userGuess);
+		}
 
-				
-			}
+		if (attempts === 0) {
+			attempts = 12;
+			losses++;
+			guess = [];
+			alert("You Lose! Click Ok to try again");
 
-			var html = 
+
+		}
+
+		var html =
 			"<h1> Psychic Game </h1>" +
-			"<p>Rules: Choose a letter on your keyboard to guess one letter from the alphabet!</p>" +
-			"<p>Wins: " + wins + "</p>" +
+			"<p>Rules: Choose a letter on your keyboard to guess one letter from the alphabet.</p>" +
+          	"<p>Wins: " + wins + "</p>" +
 			"<p>Losses: " + losses + "</p>" +
-			"<p>Guesses Left: " + numGuesses + "</p>" +
-			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+			"<p>Guesses Left: " + attempts + "</p>" +
+			"<p>Your Guesses so far: " + guess.join(", ") + "</p>";
 
-			document.querySelector("#game").innerHTML = html;
+		document.querySelector("#textcontainer").innerHTML = html;
 
-			
-			}
-		};
+
+	}
+};
